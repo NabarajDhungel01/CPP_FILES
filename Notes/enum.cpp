bf4216645c409bdf5635b508bb8ff707a446eaf0
Navy <nabaraj.dhungeel@gmail>
2022-08-11 11:03:52 +0545

    commit

---
+++ Notes/enum.cpp
@@ -0,0 +1,23 @@
+//UNIon NOTES BY CODEWITH HARRY
+
+
+#include <iostream>
+using namespace std;
+
+
+
+
+int main()
+{
+
+enum Meal{breakfast, lunch, dinner};  // here the values will be 0,1,2,3,....,n
+        // cout << endl<< breakfast;
+        // cout << endl<< lunch ;
+        // cout << endl<< dinner;
+
+
+Meal m1 = breakfast;
+cout <<m1;
+    
+return 0;
+}
---
+++ file/README.md
@@ -0,0 +1,2 @@
+# CPP_FILES
+I have put here all the CPP file that i have worked on
