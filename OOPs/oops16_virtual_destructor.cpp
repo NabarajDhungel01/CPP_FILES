4555ff8e361d06c0382fc4cfc7c8246194a3542e
NabarajDhungel01 <nabaraj.dhungeel@gmail.com>
2022-08-19 21:29:07 +0545

    commit

--- OOPs/oops16_virtual_destructor.cpp
+++ OOPs/oops16_virtual_destructor.cpp
@@ -3,16 +3,21 @@ using namespace std;
 
 
 class base{
-public:
-~base(){cout << " Base class destroyed";}};
-	
-class derived:public base{
-public:
-~derived(){cout << " derived class destroyed";}};
+        public:
+        virtual ~base(){cout << " Base class destroyed"<<endl;}};
+            
+        class derived:public base{
+        public:
+        ~derived(){cout << " derived class destroyed"<<endl;}};
 
-int main(){
-base *b = new derived; // pointer of base class and obj of der class
-delete b1;	// deleting the b1 object	// only base will be destroyed
-// we are only deleting half of the object
+        int main(){
+        base *b = new derived; // pointer of base class and obj of der class
+        delete b;	// deleting the b pointer	// only base will be destroyed
+        // we are only deleting half of the object
+
+// so to compeletely destroy the object we need to make the base virtual lets add the virtual keyword infront of the base destructor
+
+// first the derived class destructor is called then the base class destructor is calld 
+
 
 }
