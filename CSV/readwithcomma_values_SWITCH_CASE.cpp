c0b4bdd362f85cda550bdae0a963d772307ff192
Navy <nabaraj.dhungeel@gmail>
2022-09-01 08:44:42 +0545

    commit

--- CSV/readwithcomma_values_SWITCH_CASE.cpp
+++ CSV/readwithcomma_values_SWITCH_CASE.cpp
@@ -6,30 +6,34 @@ using namespace std;
 int find_length(string line); // find length of the entire line including commas
 int main()
 {
-    string csv_id, csv_name, csv_fname, csv_address, csv_phone, csv_balance, csv_pin, csv_pass;
+    string csv_id = "\0", csv_name = "\0", csv_fname = "\0", csv_address = "\0", csv_phone = "\0", csv_balance = "\0", csv_pin = "\0", csv_pass = "\0";
     string all;
     fstream fp;
     fp.open("ok.csv", ios::in);
     fp >> all;
+
     while (!fp.eof())
     {
+        int repeat_id = 0, repeat_name = 0, repeat_fname = 0, repeat_address = 0, repeat_phone = 0, repeat_balance = 0, repeat_pin = 0, repeat_pass = 0;
         int comma_found = 0;
         int position = 0;
         for (int i = 0; i < all.length(); i++)
         {
             if (all[i] == ',')
             {
+                // cout << all[i]; // prints comma
                 i++;
                 // cout << ",";
                 comma_found++;
-                position++;
+                position ++;
+                cout << " \t\t Position is :******"<<position<< "***** \t\t\n ";
             }
-            // cout << all[i];
 
             switch (position)
             {
             case 0:
                 csv_id += all[i];
+                cout << "\t\t "<<csv_id<< "\t\t ";
                 break;
             case 1:
                 csv_name += all[i];
@@ -56,23 +60,24 @@ int main()
                 cout << "Default Printed";
                 break;
             }
+            cout <<"\n###"<< all[i]<<"###";
 
-            if (comma_found != 0 && comma_found % 7 == 0 && comma_found % 6 != 0)
-            {
-                // cout << "\n";
+
+            if (comma_found != 0 && comma_found % 7 == 0 )
+            {   
+                csv_id = "\0", csv_name = "\0", csv_fname = "\0", csv_address = "\0", csv_phone = "\0", csv_balance = "\0", csv_pin = "\0", csv_pass = "\0";
+                cout << "\n";
+                cout << "=============================================================================";
             }
-
-                cout <<"\n User ID       : "<<csv_id       ;
-                cout <<"\n Name          : "<<csv_name     ;
-                cout <<"\n Father's Name : "<<csv_fname    ;
-                cout <<"\n Address       : "<<csv_address  ;
-                cout <<"\n Phone         : "<<csv_phone    ;
-                cout <<"\n Balance       : "<<csv_balance  ;
-                cout <<"\n PIN           : "<<csv_pin      ;
-                cout <<"\n Password      : "<<csv_pass     ;
-
-                cout << "----------------------------------";
-                }
+            
+            if(comma_found == 1 && repeat_id == 0)
+            {
+                cout << " ID  : "<< csv_id;
+                repeat_id = 1;
+            }
+            
+        }
+        // cout <<"\n";
         fp >> all;
     }
 
